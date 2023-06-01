0a065c01f685e710a51a92b33df4fad6a18e84a0
DESKTOP-02MEIH0\RZN <rizwandhoel@gmail.com>
2023-06-01 16:18:49 +0700

    day 8

--- javascript/testimonials.js
+++ javascript/testimonials.js
@@ -18,66 +18,129 @@ function menuBars (){
 };
 
 //OOP//
-class Testimonials{
-    #quote = '';
-    #image = '';
-    constructor(quote,image){
-        this.#quote=quote;
-        this.#image=image;
-    }
-    get quote(){
-        return this.#quote;
-    }
-    get image(){
-        return this.#image;
-    }
-    get userError(){
-        throw new Error('eror belum di isi()')
-    }
-    get TestiHml(){
-        return ` <div class="container1">
-        <div class="child-1">
-            <img src="${this.image}" alt="sekolah koding"class="photo-testi">
-            <p class="testi-user"><i>"${this.quote} !"</i></p>
-                <div class="icon">
+// class Testimonials{
+//     #quote = '';
+//     #image = '';
+//     constructor(quote,image){
+//         this.#quote=quote;
+//         this.#image=image;
+//     }
+//     get quote(){
+//         return this.#quote;
+//     }
+//     get image(){
+//         return this.#image;
+//     }
+//     get userError(){
+//         throw new Error('eror belum di isi()')
+//     }
+//     get TestiHml(){
+//         return ` <div class="container1">
+//         <div class="child-1">
+//             <img src="${this.image}" alt="sekolah koding"class="photo-testi">
+//             <p class="testi-user"><i>"${this.quote} !"</i></p>
+//                 <div class="icon">
                     
-                    <h3 class="person-testi1">- ${this.author}</h3>
-            </div>
-            </div>
-        </div>`
-    }
-}
+//                     <h3 class="person-testi1">- ${this.author}</h3>
+//             </div>
+//             </div>
+//         </div>`
+//     }
+// }
 
-    class HtmlTesti extends Testimonials{
-        #author='';
-        constructor(quote,image,author){
-            super(quote,image)
-            this.#author=author;
-        }
-        get author(){
-            return this.#author;
+//     class HtmlTesti extends Testimonials{
+//         #author='';
+//         constructor(quote,image,author){
+//             super(quote,image)
+//             this.#author=author;
+//         }
+//         get author(){
+//             return this.#author;
             
-        }
-    }
+//         }
+//     }
 
-    class Company extends Testimonials{
-        #company='';
-            constructor(quote,image,company){
-                super(quote,image)
-                this.#company=company;
-            }
-        get author(){
-            return this.#company;
-    }
-    }
-const output = new HtmlTesti('Mantap pisan','https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60','John lenon');
-const output2 = new HtmlTesti ('keren lah','https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60','Peter')
-const output3 = new HtmlTesti('Bagus cuy','https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60','Jackson');
+//     class Company extends Testimonials{
+//         #company='';
+//             constructor(quote,image,company){
+//                 super(quote,image)
+//                 this.#company=company;
+//             }
+//         get author(){
+//             return this.#company;
+//     }
+//     }
+// const output = new HtmlTesti('Mantap pisan','https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60','John lenon');
+// const output2 = new HtmlTesti ('keren lah','https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60','Peter')
+// const output3 = new HtmlTesti('Bagus cuy','https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60','Jackson');
 
-let dataUser = [output,output2,output3];
-let TestimonialsHtml='';
-for(let i=0;i<dataUser.length;i++){
-    TestimonialsHtml+=dataUser[i].TestiHml;
-}
-document.getElementById('testimonials').innerHTML=TestimonialsHtml;
+// let dataUser = [output,output2,output3];
+// let TestimonialsHtml='';
+// for(let i=0;i<dataUser.length;i++){
+//     TestimonialsHtml+=dataUser[i].TestiHml;
+// }
+// document.getElementById('testimonials').innerHTML=TestimonialsHtml;
 // console.log(output.error)
+
+//HOF//
+let dataTesti = [{
+    name:'John Lenon' ,
+    rating:5,
+    image:'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    quote:'Mantap Pisan'
+},
+{
+    name:'Peter' ,
+    rating:4,
+    image:'https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    quote:'Keren Mantap'
+},
+{
+    name:'Jackson' ,
+    rating:1,
+    image:'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    quote:'Respon Lambat',
+}];
+
+function allTestimonials(){
+    let allTesti = '';
+
+    dataTesti.forEach(function(dataItem){
+        allTesti += `<div class="container1">
+                 <div class="child-1">
+                     <img src="${dataItem.image}" alt="sekolah koding"class="photo-testi">
+                     <p class="testi-user"><i>"${dataItem.quote} !"</i></p>
+                         <div class="icon">
+                            <h3> ${dataItem.rating}<i class="ri-star-fill"></i></h3>
+                          <h3 class="person-testi1">- ${dataItem.name}</h3>
+                     </div>
+                     </div>
+                 </div>
+            `
+    })
+    document.getElementById('testimonials').innerHTML=allTesti;
+}
+allTestimonials();
+
+function testimonialsFilter(rating){
+    let testimonialsHtml ='';
+    const testimonialsFiltered = dataTesti.filter(function(item){
+        return item.rating===rating
+    })
+    if(testimonialsFiltered.length==0){
+        testimonialsHtml+=`<h1 class="data-not-found">DATA NOT FOUND</h1>`
+    }else{testimonialsFiltered.forEach(function(dataItem){
+        testimonialsHtml+=`<div class="container1">
+                 <div class="child-1">
+                     <img src="${dataItem.image}" alt="sekolah koding"class="photo-testi">
+                     <p class="testi-user"><i>"${dataItem.quote} !"</i></p>
+                         <div class="icon">
+                            <h3> ${dataItem.rating}<i class="ri-star-fill"></i></h3>
+                          <h3 class="person-testi1">- ${dataItem.name}</h3>
+                     </div>
+                     </div>
+                 </div>`
+    });
+}
+document.getElementById('testimonials').innerHTML=testimonialsHtml;
+};
