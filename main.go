80814ab636bc7372168ba13f1003ffab186ec7de
DESKTOP-02MEIH0\RZN <rizwandhoel@gmail.com>
2023-06-11 16:26:40 +0700

    day 12 golang

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 	"text/template"
@@ -19,15 +20,53 @@ func main() {
 	e.GET("/blog", blog)
 	e.GET("/testimonials", testimonials)
 	e.GET("blog-detail/:id", blogDetail)
+
 	e.POST("/add-project", addProject)
-
-	// e.GET("/", func(c echo.Context) error {
-	// 	return c.String(http.StatusOK, "Hello world!")
-
-	// })
+	e.POST("/delete-blog/:id", deleteBlog)
 	e.Logger.Fatal(e.Start("localhost:5000"))
 
 }
+
+type Blog struct {
+	Title     string
+	Content   string
+	StartDate string
+	EndDate   string
+	// Image     string
+	Author string
+	Tech1  string
+}
+
+var dataBlog = []Blog{
+	{
+		Title:     "Vue Js",
+		Content:   "Some quick example text to build on the card title and make up the bulk of the cards content.",
+		StartDate: "08/06/2023",
+		EndDate:   "09/06/2023",
+		// Image:     "public/images/img4.png",
+		Author: "Marshal",
+		Tech1:  "<h3><i class='ri-vuejs-fill'></i></h3>",
+	},
+	{
+		Title:     "Javascript",
+		Content:   "Some quick example text to build on the card title and make up the bulk of the cards content.",
+		StartDate: "08/06/2023",
+		EndDate:   "09/06/2023",
+		// Image:     "public/images/img2.png",
+		Author: "Robin Sharma",
+		Tech1:  "<h3><i class='ri-vuejs-fill'></i></h3>",
+	},
+	{
+		Title:     "Angular Js",
+		Content:   "Some quick example text to build on the card title and make up the bulk of the cards content.",
+		StartDate: "08/06/2023",
+		EndDate:   "12/12/2023",
+		// Image:     "public/images/img3.png",
+		Author: "Bill Gates",
+		Tech1:  "<h3><i class='ri-vuejs-fill'></i></h3>",
+	},
+}
+
 func helloWorld(c echo.Context) error {
 	return c.String(http.StatusOK, "hello world")
 
@@ -38,7 +77,11 @@ func home(c echo.Context) error {
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
 	}
-	return tmp.Execute(c.Response(), nil)
+	blogs := map[string]interface{}{
+		"Blogs": dataBlog,
+	}
+	return tmp.Execute(c.Response(), blogs)
+
 }
 func contact(c echo.Context) error {
 	var tmp, err = template.ParseFiles("views/contact.html")
@@ -55,16 +98,35 @@ func blog(c echo.Context) error {
 		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
 
 	}
-	return tmp.Execute(c.Response(), err)
+	return tmp.Execute(c.Response(), nil)
 }
 
 func blogDetail(c echo.Context) error {
 	id, _ := strconv.Atoi(c.Param("id"))
-	data := map[string]interface{}{
-		"Id":      id,
-		"title":   "dumbways mobile app",
-		"content": "Some quick example text to build on the card title and make up the bulk of the cards content",
+	//data := map[string]interface{}{
+	// "Id":      id,
+	// "title":   "dumbways mobile app",
+	// "content": "Some quick example text to build on the card title and make up the bulk of the cards content",
+	//	}
+	var BlogDetail = Blog{}
+
+	for i, data := range dataBlog {
+		if id == i {
+			BlogDetail = Blog{
+				Title:     data.Title,
+				StartDate: data.StartDate,
+				EndDate:   data.EndDate,
+				Content:   data.Content,
+				// Image:     data.Image,
+				Author: data.Author,
+				Tech1:  data.Tech1,
+			}
+		}
 	}
+	data := map[string]interface{}{
+		"Blog": BlogDetail,
+	}
+
 	var tmp, err = template.ParseFiles("views/blog-detail.html")
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
@@ -84,19 +146,28 @@ func addProject(c echo.Context) error {
 	date1 := c.FormValue("date1")
 	date2 := c.FormValue("date2")
 	content := c.FormValue("textarea")
-	// tech1 := c.FormValue("reactJs")
-	// tech2 := c.FormValue("angularJs")
-	// tech3 := c.FormValue("vueJs")
-	// tech4 := c.FormValue("javascript")
-
-	println("Title	:" + title)
+	println("author	:" + title)
 	println("start date :" + date1)
 	println("end date :" + date2)
 	println("description:" + content)
-	// println("technologi 1:" + tech1)
-	// println("technologi 2:" + tech2)
-	// println("technologi 3:" + tech3)
-	// println("technologi 4:" + tech4)
 
-	return c.Redirect(http.StatusMovedPermanently, "/blog")
+	var newBlog = Blog{
+		Title:     title,
+		StartDate: date1,
+		EndDate:   date2,
+		Content:   content,
+		Author:    "user",
+	}
+
+	dataBlog = append(dataBlog, newBlog)
+	fmt.Println(dataBlog)
+
+	return c.Redirect(http.StatusMovedPermanently, "/index")
+}
+func deleteBlog(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	fmt.Println("Deleted :", id)
+	dataBlog = append(dataBlog[:id], dataBlog[id+1:]...)
+
+	return c.Redirect(http.StatusMovedPermanently, "/index")
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,18 +1,23 @@
-let navMenu = false;
-let clickIcon = document.getElementById('burgerMenu');
-let firstContainer = document.getElementById('firstContainer');
-let getBars = document.getElementById('containerHamburger');
-function burgerNavBars(){
-    if(navMenu==false){
-        getBars.style.display='block';
-        clickIcon.style.transform='rotate(90deg)'
-        firstContainer.style.marginTop='200px';
-        firstContainer.style.transition='.8s'
-        navMenu=true;
+// let navMenu = false;
+// let clickIcon = document.getElementById('burgerMenu');
+// let firstContainer = document.getElementById('firstContainer');
+// let getBars = document.getElementById('containerHamburger');
+// function burgerNavBars(){
+//     if(navMenu==false){
+//         getBars.style.display='block';
+//         clickIcon.style.transform='rotate(90deg)'
+//         firstContainer.style.marginTop='200px';
+//         firstContainer.style.transition='.8s'
+//         navMenu=true;
       
-    }else{getBars.style.display='none';
-        firstContainer.style.marginTop='80px';
-        clickIcon.style.transform='rotate(0)'
-        navMenu=false;
-}
+//     }else{getBars.style.display='none';
+//         firstContainer.style.marginTop='80px';
+//         clickIcon.style.transform='rotate(0)'
+//         navMenu=false;
+// }
+// }
+function button (){
+    alert('Removed')
+    // event.preventDefault()
+let btn = document.getElementById("childCard1")
 }
